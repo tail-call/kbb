0b11c2af781dbede806bb8743c06bb7232c7a371
Maria I <maria@tail-call.ru>
2023-02-19 14:35:58 +0700

    Microoptimizations are a root of all good

--- canvas.lua
+++ canvas.lua
@@ -1,6 +1,6 @@
 local function withCanvas(canvas, cb)
   love.graphics.setCanvas(canvas)
-  love.graphics.push()
+  love.graphics.push('transform')
   love.graphics.replaceTransform(love.math.newTransform())
   cb()
   love.graphics.pop()
