1e5f08ef57bf3a2004b1a870a41af3bfe5aa2ad5
Maria I <maria@tail-call.ru>
2023-04-20 14:52:59 +0700

    Move Console to console.lua

---
+++ console.lua
@@ -0,0 +1,45 @@
+---@class ConsoleMessage Message in the bottom console
+---@field text string Message text
+---@field lifetime number Seconds before message is faded out
+---@field fadeOut fun(self: ConsoleMessage, dt: number) Applies fade out to the message
+
+---@class Console Bottom console
+---@field messages ConsoleMessage[] List of displayed messages
+---@field say fun(self: Console, message: ConsoleMessage) Displays a message
+---@field removeTopMessage fun(self: Console) Removes a topmost message
+--
+---@param text string
+---@param lifetime number
+---@return ConsoleMessage
+local function makeConsoleMessage(text, lifetime)
+  ---@type ConsoleMessage
+  local message = {
+    text = text,
+    lifetime = lifetime,
+    fadeOut = function (self, dt)
+      self.lifetime = math.max(self.lifetime - dt, 0)
+    end,
+  }
+  return message
+end
+
+---@param messages ConsoleMessage[]
+---@return Console
+local function makeConsole(messages)
+  ---@type Console
+  local console = {
+    messages = messages,
+    say = function (self, message)
+      table.insert(self.messages, message)
+    end,
+    removeTopMessage = function (self)
+      table.remove(self.messages, 1)
+    end
+  }
+  return console
+end
+
+return {
+  makeConsole = makeConsole,
+  makeConsoleMessage = makeConsoleMessage,
+}
--- game.lua
+++ game.lua
@@ -13,6 +13,8 @@ local vector = require('./vector')
 local rng = require('./rng')
 local ability = require('./ability')
 local maybeDrop = require('./tbl').maybeDrop
+local makeConsoleMessage = require('./console').makeConsoleMessage
+local makeConsole = require('./console').makeConsole
 
 ---@class Building
 ---@field pos Vector Building's position
@@ -24,16 +26,6 @@ local maybeDrop = require('./tbl').maybeDrop
 ---@field maybeGrow fun(self: RecruitCircle, dt: number)
 ---@field reset fun(self: RecruitCircle)
 ---@field clear fun(self: RecruitCircle)
-
----@class ConsoleMessage Message in the bottom console
----@field text string Message text
----@field lifetime number Seconds before message is faded out
----@field fadeOut fun(self: ConsoleMessage, dt: number) Applies fade out to the message
-
----@class Console Bottom console
----@field messages ConsoleMessage[] List of displayed messages
----@field say fun(self: Console, message: ConsoleMessage) Displays a message
----@field removeTopMessage fun(self: Console) Removes a topmost message
 
 ---@class Squad
 ---@field shouldFollow boolean True if guys should follow the player
@@ -140,37 +132,6 @@ local SCORES_TABLE = {
 local NONE_COLLISION = { type = 'none' }
 ---@type CollisionInfo
 local TERRAIN_COLLISION = { type = 'terrain' }
-
----@param text string
----@param lifetime number
----@return ConsoleMessage
-local function makeConsoleMessage(text, lifetime)
-  ---@type ConsoleMessage
-  local message = {
-    text = text,
-    lifetime = lifetime,
-    fadeOut = function (self, dt)
-      self.lifetime = math.max(self.lifetime - dt, 0)
-    end,
-  }
-  return message
-end
-
----@param messages ConsoleMessage[]
----@return Console
-local function makeConsole(messages)
-  ---@type Console
-  local console = {
-    messages = messages,
-    say = function (self, message)
-      table.insert(self.messages, message)
-    end,
-    removeTopMessage = function (self)
-      table.remove(self.messages, 1)
-    end
-  }
-  return console
-end
 
 ---@return Resources
 local function makeResources()
