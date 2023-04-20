cf55dcb40544fc1900eb823739d7d82567954230
Maria I <maria@tail-call.ru>
2023-04-20 20:20:45 +0700

    Merge font.lua into util.lua

--- util.lua
+++ util.lua
@@ -58,6 +58,49 @@ local function randomLetterCode()
   return string.byte(letters, idx)
 end
 
+-- See <https://love2d.org/wiki/ImageFontFormat>
+
+---@param name string
+---@param charWidth integer
+---@param charHeight integer
+---@param isBold boolean
+local function loadFont(name, charWidth, charHeight, isBold)
+  local data = love.image.newImageData(name)
+  local characters = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+  local fontWidth = 1 + #characters * (charWidth + 1)
+  local output = love.image.newImageData(fontWidth, charHeight)
+
+  local function putSeparator(x)
+    for y = 0, charHeight - 1 do
+      output:setPixel(x, y, 1, 0, 1, 1)
+    end
+  end
+
+  local base = (isBold and 256 or 0) + 32 -- base 0 + 32 for non-bold font
+  local offset = 0
+  for char = base, base + #characters do
+    putSeparator(offset)
+    local y = math.floor(char / 32)
+    local x = char % 32
+
+    output:paste(
+      data, -- Source
+      offset + 1, -- dx
+      0, -- dy
+      charWidth * x, -- sx
+      charHeight * y, -- sy
+      charWidth, -- sw
+      charHeight -- sh
+    )
+
+    offset = offset + charWidth + 1
+  end
+
+  putSeparator(fontWidth - 1)
+
+  return love.graphics.newImageFont(output, characters)
+end
+
 return {
   exhaust = exhaust,
   withCanvas = withCanvas,
@@ -65,4 +108,5 @@ return {
   withLineWidth = withLineWidth,
   withTransform = withTransform,
   randomLetterCode = randomLetterCode,
+  loadFont = loadFont,
 }
