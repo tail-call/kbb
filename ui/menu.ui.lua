c33a509f474ab82070a03d5a7bd845cc55dafea9
Maria I <maria@tail-call.ru>
2023-05-26 15:19:05 +0700

    Rainbow menu

--- ui/menu.ui.lua
+++ ui/menu.ui.lua
@@ -31,6 +31,29 @@ F) Reload this menu
 Q) Quit game
 ]]
 
+-- From <https://love2d.org/wiki/HSV_color>
+local function hsv(h, s, v)
+  if s <= 0 then return v,v,v end
+  h = h * 6
+  local c = v*s
+  local x = (1-math.abs((h%2)-1))*c
+  local m,r,g,b = (v-c), 0, 0, 0
+  if h < 1 then
+      r, g, b = c, x, 0
+  elseif h < 2 then
+      r, g, b = x, c, 0
+  elseif h < 3 then
+      r, g, b = 0, c, x
+  elseif h < 4 then
+      r, g, b = 0, x, c
+  elseif h < 5 then
+      r, g, b = x, 0, c
+  else
+      r, g, b = c, 0, x
+  end
+  return { r = r + m, g = g + m, b = b + m, a = 1 }
+end
+
 return {
   Panel {
     transform = function () return Origin() end,
@@ -42,12 +65,11 @@ return {
         .. (Model.cursorTimer > 0.5 and '_' or '')
     end,
     Background(function()
-      return {
-        r = (love.timer.getTime() % 1) / 2,
-        g = 0.3,
-        b = 0.1,
-        a = 1
-      }
+      return hsv(
+        (love.timer.getTime() / 4) % 1,
+        0.5,
+        0.5
+      )
     end),
   },
 }
