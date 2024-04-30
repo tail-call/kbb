df361bfe731a5fb123d448d2edac9e2615ef3354
Maria I <maria@tail-call.ru>
2024-05-01 00:45:00 +0700

    Global.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,4 @@
----Global variables. `main.lua` defines default values, `conf.lua`
----contains actual configuration.
-Global = {
-  ---If true, warning functions from `core.log` module will crash the program
-  shouldCrashOnWarnings = false,
-  ---If true, warnings will be printed to the console
-  shouldLogWarnings = true,
-  ---Module to be required and used as a starting scene
-  initialScene = '<not a scene>',
-  ---Default scale of the graphics
-  defaultGraphicsScale = 2,
-  ---Where to spawn the leader at when she dies
-  leaderSpawnLocation = { x = 250, y = 250 },
-}
-
+dofile 'Global.lua'
 dofile 'conf.lua'
 
 Class = require 'core.class'.defineClass
