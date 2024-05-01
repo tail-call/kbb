e018817c27e700c18c7265fb40a7c4483bee7f34
Maria I <maria@tail-call.ru>
2024-05-02 00:35:54 +0700

    Mention wonder

--- ui/screen.lua
+++ ui/screen.lua
@@ -87,7 +87,7 @@ SetModel {
 
     local controls = ''
     if game.mode == 'normal' then
-      controls = 'Space] paint\nLMB] recruit\n8] save\nZ] zoom\nF] follow\nQ] gather\nT] warp\nC] collect\nX] fly\nH] respawn\n'
+      controls = 'Space] paint\nLMB] recruit\n8] save\nZ] zoom\nF] follow\nE] wonder\nQ] gather\nT] warp\nC] collect\nX] fly\nH] respawn\n'
     elseif game.mode == 'paint' then
       controls = 'Space] focus\nLMB] paint\n'
     end
